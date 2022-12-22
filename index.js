250c728b35686a419556629cdebec88bb0c35806
Aléxia Balionis <alexia.balionis@olxbr.com>
2022-12-22 14:05:17 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const github = require("@actions/github");
+const core = require("@actions/core");
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken = core.getInput("token");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+  const octokit = github.getOctokit(myToken);
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+  const { data } = await octokit.rest.pulls.list({
+    owner: "olxbr",
+    repo: "listing",
+    state: "close",
+  });
+
+  // const { data: pullRequest } = await octokit.rest.pulls.get({
+  //   owner: "octokit",
+  //   repo: "rest.js",
+  //   pull_number: 123,
+  //   mediaType: {
+  //     format: "diff",
+  //   },
+  // });
+
+  console.log(data);
 }
 
 run();
